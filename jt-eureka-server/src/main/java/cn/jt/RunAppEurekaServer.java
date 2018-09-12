0e3678176545c6013c6928dc813c1189d9a67eb4
qiufengluoye110110 <1071786198@qq.com>
2018-09-12 14:13:43 +0800

    jtspringcloud

---
+++ jt-eureka-server/src/main/java/cn/jt/RunAppEurekaServer.java
@@ -0,0 +1,14 @@
+package cn.jt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer//启动Eureka
+public class RunAppEurekaServer {
+public static void main(String[] args) {
+	SpringApplication.run(RunAppEurekaServer.class, args);
+}
+	
+}
---
+++ jt-gateway-zuul/src/main/java/cn/jt/RunAppGatewayZuul.java
@@ -0,0 +1,19 @@
+package cn.jt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class RunAppGatewayZuul {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunAppGatewayZuul.class, args);
+	}
+	
+	
+	
+}
